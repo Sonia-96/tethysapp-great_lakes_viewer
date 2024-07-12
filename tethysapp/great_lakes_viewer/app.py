b15874dda31547e58bc7f8f87ea2fceeae664cff
Sonia-96 <sypku1996@gmail.com>
2024-07-12 17:42:08 -0600

    create the app

---
+++ tethysapp/great_lakes_viewer/app.py
@@ -0,0 +1,18 @@
+from tethys_sdk.base import TethysAppBase
+
+
+class GreatLakesViewer(TethysAppBase):
+    """
+    Tethys app class for Great Lakes Viewer.
+    """
+
+    name = 'Great Lakes Viewer'
+    description = ''
+    package = 'great_lakes_viewer'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/icon.gif'
+    root_url = 'great-lakes-viewer'
+    color = '#2980b9'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
