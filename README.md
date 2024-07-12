ad56710dd1edb2ff2b112ddef73ad906bb0830c0
Sonia-96 <sypku1996@gmail.com>
2024-07-12 17:55:49 -0600

    update readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Greate Lake Viewer
+
+![image-app](./images/greaet_lakes_viewer.PNG)
+![image-plot](./images/greaet_lakes_viewer-app.PNG)
--- tethysapp/great_lakes_viewer/controllers.py
+++ tethysapp/great_lakes_viewer/controllers.py
@@ -10,7 +10,6 @@ class GreatLakesViewer(MapLayout):
     app = app
     base_template = 'great_lakes_viewer/base.html'
     map_title = 'Great Lakes Viewer'
-    map_subtitle = 'xxx'
     
     basemaps=["OpenStreetMap"]
     default_map_extent = [-95.48678973290308, 39.469776324236335, -71.79882218561728, 51.10826350669163]
